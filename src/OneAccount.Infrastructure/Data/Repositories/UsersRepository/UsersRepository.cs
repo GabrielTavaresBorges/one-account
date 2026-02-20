2d2311f4bc10f51ec97dc4cbda46b267041d5fa4
GabrielTavaresB <gabrieltavaresborges@hotmail.com>
2026-02-19 21:45:36 -0300

    feat: camada de infraestrutura inicial

---
+++ src/OneAccount.Infrastructure/Data/Repositories/UsersRepository/UsersRepository.cs
@@ -0,0 +1,13 @@
+﻿using OneAccount.Domain.Entities.Users;
+using OneAccount.Domain.Repositories.UsersRepository;
+using OneAccount.Infrastructure.Data.Context;
+
+namespace OneAccount.Infrastructure.Data.Repositories.UserRepository;
+
+public sealed class UsersRepository(AppDbContext context) : IUsersRepository
+{
+    public async Task CreateUserAsync(User user, CancellationToken cancellationToken)
+    {
+        await context.Users.AddAsync(user, cancellationToken);
+    }
+}
